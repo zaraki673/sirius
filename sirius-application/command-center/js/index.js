178421b4ed4df51804161c493ebc74f9692c3f27
Tim W <twurman@umich.edu>
2015-06-06 15:11:52 -0400

    swapped to binary protocol

--- sirius-application/command-center/js/index.js
+++ sirius-application/command-center/js/index.js
@@ -3,7 +3,7 @@ function pingServer(){
     var addr = '//localhost:8081';
     console.log(addr);
     var transport = new Thrift.Transport(addr);
-    var protocol  = new Thrift.Protocol(transport);
+    var protocol  = new Thrift.TBinaryProtocol(transport);
     var client    = new CommandCenterClient(protocol);
     client.ping();
     console.log("Client Created");
