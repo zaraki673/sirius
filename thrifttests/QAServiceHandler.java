d8cd9dbe5306002335a346bfaadb453a14071c75
Ben Toll <tollben@umich.edu>
2015-06-07 10:44:17 -0400

    removed thrifttests/ from repo

--- thrifttests/QAServiceHandler.java
+++
@@ -1,8 +0,0 @@
-import tutorial.QAService;
-import java.util.List;
-public class QAServiceHandler implements QAService.Iface {
-	public void AskQuestion(List<String> question) {
-		System.out.println("Java server has finished processing your request...");
-	}
-}
-
--- thrifttests/qaservice.thrift
+++
@@ -1,9 +0,0 @@
-# thrift compiler translates each service into .cpp/.h files
-# clients include these files so that they can serialize their args in the correct form
-# servers include these files so that they can serialize return values in the correct form
-namespace java tutorial
-
-service QAService
-{
-	void AskQuestion(1:list<string> question)
-}
