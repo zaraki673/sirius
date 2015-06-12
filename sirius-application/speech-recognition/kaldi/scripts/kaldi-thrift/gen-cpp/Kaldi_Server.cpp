1560955f03a4f5998ded1c51b748922fd9f14727
Moeiz Riaz <moeizr@umich.edu>
2015-06-12 11:50:47 -0400

    Reordered some files for node.js server into command-center

--- sirius-application/speech-recognition/kaldi/scripts/kaldi-thrift/gen-cpp/Kaldi_Server.cpp
+++ sirius-application/speech-recognition/kaldi/scripts/kaldi-thrift/gen-cpp/Kaldi_Server.cpp
@@ -21,10 +21,10 @@
 
 #include "KaldiService.h"
 #include "subproc.h"
-//#include "CommandCenter.h"
-//#include "commandcenter_types.h"
-#include "/home/momo/Research/sirius/sirius-application/command-center/gen-cpp/CommandCenter.h"
-#include "/home/momo/Research/sirius/sirius-application/command-center/gen-cpp/commandcenter_types.h"
+#include "CommandCenter.h"
+#include "commandcenter_types.h"
+//#include "/home/momo/Research/sirius/sirius-application/command-center/gen-cpp/CommandCenter.h"
+//#include "/home/momo/Research/sirius/sirius-application/command-center/gen-cpp/commandcenter_types.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
