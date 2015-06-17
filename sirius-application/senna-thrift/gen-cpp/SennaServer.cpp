6ff89fca333dc590dbab80acef5e85864709abce
VivienZhou <tiantong@umich.edu>
2015-06-17 16:40:30 -0400

    request handler

--- sirius-application/senna-thrift/gen-cpp/SennaServer.cpp
+++ sirius-application/senna-thrift/gen-cpp/SennaServer.cpp
@@ -1,6 +1,30 @@
 /*
 Derived from main.cpp in DjiNN
 */
+//thread.cpp
+#include <sstream>
+#include <assert.h>
+#include <stdint.h>
+#include <ctime>
+#include <cmath>
+#include <boost/chrono/thread_clock.hpp>
+#include "timer.h"
+
+extern map<string, Net<float>*> nets;
+extern bool debug;
+extern bool gpu;
+
+//thread.h
+#include <vector>
+#include "caffe/caffe.hpp"
+
+using caffe::Blob;
+using caffe::Caffe;
+using caffe::Net;
+using caffe::Layer;
+using caffe::shared_ptr;
+using caffe::Timer;
+using caffe::vector;
 
 #include "SennaService.h"
 #include <thrift/protocol/TBinaryProtocol.h>
@@ -28,8 +52,6 @@ Derived from main.cpp in DjiNN
 #include "SENNA_POS.h"
 #include "SENNA_CHK.h"
 #include "SENNA_NER.h"
-
-#include "socket.h"
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -138,6 +160,101 @@ class SennaServiceHandler : virtual public SennaServiceIf {
   	}
 	strcpy(app.pl.req_name, app.task.c_str());
   }  
+
+  //handler request
+  //helper function
+  void SERVICE_fwd(float* in, int in_size, float* out, int out_size,
+                 Net<float>* net) {
+	string net_name = net->name();
+  	STATS_INIT("service", "DjiNN service inference");
+  	PRINT_STAT_STRING("network", net_name.c_str());
+
+  	if (Caffe::mode() == Caffe::CPU)
+   	 PRINT_STAT_STRING("platform", "cpu");
+  	else
+   	 PRINT_STAT_STRING("platform", "gpu");
+
+  	float loss;
+  	vector<Blob<float>*> in_blobs = net->input_blobs();
+
+  	tic();
+  	in_blobs[0]->set_cpu_data(in);
+ 	 vector<Blob<float>*> out_blobs = net->ForwardPrefilled(&loss);
+  	memcpy(out, out_blobs[0]->cpu_data(), sizeof(float));
+
+  	PRINT_STAT_DOUBLE("inference latency", toc());
+
+  	STATS_END();
+
+  	if (out_size != out_blobs[0]->count())
+    		LOG(FATAL) << "out_size =! out_blobs[0]->count())";
+  	else
+    		memcpy(out, out_blobs[0]->cpu_data(), out_size * sizeof(float));
+ }	
+
+  //string/char* req_name?
+  void* request_handler(string req_name, char * data){
+	 map<string, Net<float>*>::iterator it = nets.find(req_name);
+ 	 if (it == nets.end()) {
+   		 LOG(ERROR) << "Task " << req_name << " not found.";
+   		 return (void*)1;
+ 	 } else
+ 		 LOG(INFO) << "Task " << req_name << " forward pass.";
+	 
+	 // reshape input dims if incoming data != current net config
+	LOG(INFO) << "Elements received on socket " << sock_elts << endl;
+
+	reshape(nets[req_name], sock_elts);
+
+	int in_elts = nets[req_name]->input_blobs()[0]->count();
+	int out_elts = nets[req_name]->output_blobs()[0]->count();
+	float* in = (float*)malloc(in_elts * sizeof(float));
+	float* out = (float*)malloc(out_elts * sizeof(float));
+	
+	// Main loop of the thread, following this order
+	// 1. Receive input feature (has to be in the size of sock_elts)
+	// 2. Do forward pass
+	// 3. Send back the result
+	// 4. Repeat 1-3
+
+	// Warmup: used to move the network to the device for the first time
+	// In all subsequent forward passes, the trained model resides on the
+	// device (GPU)
+	bool warmup = true;
+ 
+	while (1) {
+		LOG(INFO) << "Reading from socket.";
+		//socket_send
+		//type cast: float* in, void* data
+		in = (char*) data;
+	
+    		if (warmup && gpu) {
+			float loss;
+      			vector<Blob<float>*> in_blobs = nets[req_name]->input_blobs();
+      			in_blobs[0]->set_cpu_data(in);
+      			vector<Blob<float>*> out_blobs;
+      			out_blobs = nets[req_name]->ForwardPrefilled(&loss);
+      			warmup = false;
+   		 }	
+
+   		 LOG(INFO) << "Executing forward pass.";
+   		 SERVICE_fwd(in, in_elts, out, out_elts, nets[req_name]);
+
+    		LOG(INFO) << "Writing to socket.";
+		//out is sth that need to be sent back 
+    		SOCKET_send(socknum, (char*)out, out_elts * sizeof(float), debug);
+   		 //socket_receive
+      }	
+
+	// Exit the thread
+	LOG(INFO) << "Socket closed by the client.";
+
+	free(in);
+	free(out);
+
+	return (void*)0;
+  }
+  
 
   //Member data
   
